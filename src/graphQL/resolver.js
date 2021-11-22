92c00ac740162e1d5ef54a385c50e348bf0e4561
wiseley <spezuluaga@gmail.com>
2021-11-22 09:35:30 -0500

    Divide services, Single Responsibility Principle (SRP)

--- src/graphQL/resolver.js
+++ src/graphQL/resolver.js
@@ -1,55 +1,45 @@
-const Project = require('../models/projectModel');
-const User = require('../models/userModel');
 const aes256 = require('aes256');
+
+const {
+    projects,
+    getProject,
+    createProject,
+    stopProject,
+    resumeProject,
+    addUserToProject
+} = requiere('../service/project.service')
+
+const {
+    users,
+    getUser,
+    activateUser,
+    deleteUser,
+    createUser
+} = require('../service/user.service')
+
+
+
 
 const key = 'Clave_para_aes256';
 
 const resolvers = {
     Query: {
-        users: async ()=> await User.find({}),
-        projects: async () => await Project.find({})
+        users: async ()=> users(),
+        getUser: async (parent, args, context, info) => getUser(args.id),
+
+        projects: async () => projects(),
+        getProject: async (parent, args, context, info) => getProject(args.title)
     },
 
+
     Mutation:{
-        createUser: (parent, args, context, info) => {
-            const {password} = args.user;
-            const newUser = new User(args.user);
-            const encryptedPassword = aes256.encrypt(key, password);
-            newUser.password = encryptedPassword;
-            return newUser.save()
-                .then (u => "user created")
-                .catch(err => "Failed")
-        },
+        createUser: (parent, args, context, info) => createUser(args.user),
+        activateUser: async (parent, args, context, info) => activateUser(args.id),
+        deleteUser: async (parent, args, context, info) => deleteUser(args.id),
 
-        activateUser: async (parent, args, context, info) => {
-            return User.updateOne({id:args.id}, {accountStatus:"Activa"})
-                .then(u => "Update: Successful")
-                .catch(err => "Update: Failed")
-        },
-
-        deleteUser: async (parent, args, context, info) => {
-            return User.deleteOne({id:args.id})
-            .then(u => "User eliminated")
-            .catch(err => "Failed")
-        },
-
-        createProject: (parent, args, context, info) => {
-            const newProject = new Project(args.project);
-            return newProject.save()
-                .then(u => "Project: Created")
-                .catch(err => "Project: Error")
-        },
-
-        stopProject: async (parent, args, context, info) => {
-            return Project.updateOne({title:args.title}, {$set:{isActive:false}})
-                .then(u => "Project: Stopped")
-                .catch(err => "Project: Error")
-        },
-        resumeProject: async (parent, args, context, info) => {
-            return Project.updateOne({title:args.title}, {$set:{isActive:true}})
-                .then(u => "Project: Resumed")
-                .catch(err => "Project: Error")
-        }
+        createProject: (parent, args, context, info) => createProject(args.project),
+        stopProject: async (parent, args, context, info) => stopProject(args.title),
+        resumeProject: async (parent, args, context, info) => resumeProject(args.title)
     }
 }
 
